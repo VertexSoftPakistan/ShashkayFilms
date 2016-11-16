ffd88009b067f0d5c101ae395b8d07fc306a4cc3
Nauman Iftikhar <naumanpices88@gmail.com>
2016-11-16 12:08:26 +0500

    Initial Commit

---
+++ ShashkayFilms/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShashkayFilms
+//
+//  Created by Nauman on 16/11/2016.
+//  Copyright © 2016 VertexSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ShashkayFilms/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ShashkayFilms
+//
+//  Created by Nauman on 16/11/2016.
+//  Copyright © 2016 VertexSoft. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
